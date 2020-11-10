d6c19a9caf7409f967f1dc40eaedc7199d5aef88
Archana <arch.2421@gmail.com>
2020-11-10 21:31:00 +0530

    Ranking for Wildcard queries

--- ranking.py
+++ ranking.py
@@ -21,8 +21,13 @@ class Ranking:
         length_tq = 0
 
         for term in query:
-            tot_score += (index_trie[term][docid][1] * tfidf_query[term]) # tf-idf w.r.t document * tf-idf w.r.t query
-            length_td += (index_trie[term][docid][1] * index_trie[term][docid][1])
+            if term in index_trie and docid in index_trie[term]:
+                tfidf_doc = index_trie[term][docid][1]
+            else:
+                tfidf_doc = 0
+
+            tot_score += (tfidf_doc * tfidf_query[term]) # tf-idf w.r.t document * tf-idf w.r.t query
+            length_td += (tfidf_doc * tfidf_doc)
             length_tq += (tfidf_query[term] * tfidf_query[term])
 
         tot_score = tot_score / (math.sqrt(length_tq) * math.sqrt(length_td))
@@ -30,21 +35,41 @@ class Ranking:
 
     # ---------------------------------------- RANK RESULTS -----------------------------------------------------------
     
-    def rank_all(self, query, answers, indexes, idf_dict):
+    def rank_all(self, query, answers, indexes, idf_dict, isWC = 0):
         """ Ranks final list of documents based on tf-idf scores """
-
+        
         final = list()
         tfidf_query = dict()
+        
+        if isWC: # wildcard
+            new_query = list()
+
+            for term in query:
+                if '*' not in term:
+                    new_query.append(term)
+            
+            if len(new_query) == 0: # if query has only WC terms, return all matching docids with same score 1
+                for index_num in answers:
+                    for docid in answers[index_num]:
+                        final.append([docid, 1, index_num]) # score: [docid, score of document, index_num]
+        
+                return final   
+            
+            query = new_query     
+         
+        
+
         q = set(query)
         
         for index_num in answers:
         
             # tf-idf of each query term w.r.t the query
             for term in q:
-                if term in idf_dict[index_num]:
+                if term in idf_dict[index_num]: # {i:{term:idf}}
                     qc = math.log(1 + query.count(term), 10) #log normalization of term count in query
                     tfidf_query[term] = idf_dict[index_num][term] * qc
-            
+                else:
+                    tfidf_query[term] = 0
             # computing score for each matching document
             for docid in answers[index_num]:
                 score = self.compute_score(query, docid, indexes[index_num][0], tfidf_query)
